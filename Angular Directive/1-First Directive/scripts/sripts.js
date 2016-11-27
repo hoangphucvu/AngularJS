57b52bd37f0b4adfef3efee2ad9cc0fd36213f01
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-11-27 15:40:07 +0700

    Setup project

---
+++ Angular Directive/1-First Directive/scripts/sripts.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Ngo Hung Phuc
+ * @Date:   2016-11-27 15:36:00
+ * @Last Modified by:   Ngo Hung Phuc
+ * @Last Modified time: 2016-11-27 15:39:40
+ */
+/**
+ * app Module
+ *
+ * Description
+ */
+angular.module('app', []);
+
+angular.module('app', []).controller('mainCtrl', function($scope) {
+
+});
