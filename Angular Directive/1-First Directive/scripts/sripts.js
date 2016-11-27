9403cb3652d4ce30b2ea181a119bdf643cec4057
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-11-27 15:56:54 +0700

    User info directive

--- Angular Directive/1-First Directive/scripts/sripts.js
+++ Angular Directive/1-First Directive/scripts/sripts.js
@@ -2,15 +2,27 @@
  * @Author: Ngo Hung Phuc
  * @Date:   2016-11-27 15:36:00
  * @Last Modified by:   Ngo Hung Phuc
- * @Last Modified time: 2016-11-27 15:39:40
+ * @Last Modified time: 2016-11-27 15:56:23
  */
 /**
  * app Module
  *
  * Description
+ * Restrict A stand for Attribute
+ * E for element
+ * C for class
+ * M for comment
  */
 angular.module('app', []);
 
 angular.module('app', []).controller('mainCtrl', function($scope) {
 
+});
+
+
+angular.module('app', []).directive('userInfoCard', function() {
+    return {
+        template: "User Info Card",
+        restrict: "E"
+    };
 });
