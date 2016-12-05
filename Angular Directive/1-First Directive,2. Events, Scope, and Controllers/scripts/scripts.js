804f61ee80cf9c8e0def94b30ed4ea02dcd93da1
hoangphucvu <ngohungphuc95@gmail.com>
2016-12-05 13:41:18 +0700

    Isolated scope function params

--- Angular Directive/1-First Directive,2. Events, Scope, and Controllers/scripts/scripts.js
+++ Angular Directive/1-First Directive,2. Events, Scope, and Controllers/scripts/scripts.js
@@ -15,7 +15,7 @@
  */
 angular.module('app', []);
 
-angular.module('app').controller('mainCtrl', function($scope) {
+angular.module('app').controller('mainCtrl', function ($scope) {
     $scope.user1 = {
         name: 'Luke Skywalker',
         address: {
@@ -33,42 +33,70 @@ angular.module('app').controller('mainCtrl', function($scope) {
 });
 
 
-angular.module('app').directive('userInfoCard', function() {
+angular.module('app').directive('userInfoCard', function () {
     return {
         restrict: 'E',
         templateUrl: 'userInfoCard.html',
         //scope: true,//internal scope
-        //@ rename attribute
-        //=person customize for html attribute tag
+        //@ Attribute string binding
+        //= Two-way model binding
+        //& Callback method binding
         scope: {
             user: '=person',
             initialCollapsed: '@collapsed'
         },
-        controller: function($scope) {
+        controller: function ($scope) {
             $scope.collapsed = ($scope.initialCollapsed === 'true');
-            $scope.knightMe = function(user) {
+            $scope.knightMe = function (user) {
                 user.rank = "knight";
             };
-            $scope.collapse = function() {
+            $scope.collapse = function () {
                 //set to it's opposite value of currently is
                 $scope.collapsed = !$scope.collapsed;
+            };
+            $scope.removeFriend = function (friend) {
+                var index = $scope.user.friends.indexOf(friend);
+                if (index > -1) {
+                    $scope.user.friends.splice(index, 1);
+                }
             };
         }
     };
 });
-
-angular.module('app').directive('address', function() {
+angular.module('app').directive('removeFriend', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'removeFriend.html',
+        //create isolate scope
+        scope: {
+            notifyParent: '&method'
+        },
+        controller: function ($scope) {
+            $scope.removing = false;
+            $scope.startRemove = function () {
+                $scope.removing = true;
+            };
+            $scope.cancelRemove = function () {
+                $scope.removing = false;
+            };
+            $scope.confirmRemove = function () {
+                $scope.notifyParent();
+            };
+        }
+    }
+});
+angular.module('app').directive('address', function () {
     return {
         restrict: 'E',
         templateUrl: 'address.html',
         scope: true,
-        controller: function($scope) {
+        controller: function ($scope) {
             $scope.collapsed = false;
-            $scope.collapseAddress = function() {
+            $scope.collapseAddress = function () {
                 //set to it's opposite value of currently is
                 $scope.collapsed = true;
             };
-            $scope.expandAddress = function() {
+            $scope.expandAddress = function () {
                 //set to it's opposite value of currently is
                 $scope.collapsed = false;
             };
